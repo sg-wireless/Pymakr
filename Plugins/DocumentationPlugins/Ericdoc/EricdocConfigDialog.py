ff4b216aa06760b45fcb70b7c5e09025cdd898f2
Detlev Offenbach <detlev@die-offenbachs.de>
2016-02-22 18:42:34 +0100

    Fixed an issue in the eric doc configuration dialog.

--- Plugins/DocumentationPlugins/Ericdoc/EricdocConfigDialog.py
+++ Plugins/DocumentationPlugins/Ericdoc/EricdocConfigDialog.py
@@ -193,6 +193,7 @@ class EricdocConfigDialog(QDialog, Ui_EricdocConfigDialog):
         else:
             self.parameters['outputDirectory'] = \
                 self.defaults['outputDirectory']
+            parms['outputDirectory'] = self.parameters['outputDirectory']
         if self.parameters['ignoreDirectories'] != \
                 self.defaults['ignoreDirectories']:
             parms['ignoreDirectories'] = \
