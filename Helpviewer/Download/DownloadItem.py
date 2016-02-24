c1c63c939154cce3592cc4cf3e1b8004cacb5270
Detlev Offenbach <detlev@die-offenbachs.de>
2016-02-24 20:28:12 +0100

    Fixed an issue in the download item related to Qt5.

--- Helpviewer/Download/DownloadItem.py
+++ Helpviewer/Download/DownloadItem.py
@@ -213,8 +213,8 @@ class DownloadItem(QWidget, Ui_DownloadItem):
             self.__autoOpen = dlg.getAction() == "open"
             if PYQT_VERSION_STR >= "5.0.0":
                 from PyQt5.QtCore import QStandardPaths
-                tempLocation = QStandardPaths.storageLocation(
-                    QStandardPaths.TempLocation)
+                tempLocation = QStandardPaths.standardLocations(
+                    QStandardPaths.TempLocation)[0]
             else:
                 from PyQt5.QtGui import QDesktopServices
                 tempLocation = QDesktopServices.storageLocation(
