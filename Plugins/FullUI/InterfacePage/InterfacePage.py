2ca09fd0419d0ba7f6df2d31b4860e46eadf8ebf
Ralph Hogenbirk <ralph@pycom.io>
2016-10-05 16:55:48 +0200

    Added new combo box for dark/light choice (not working yet)

--- Plugins/FullUI/InterfacePage/InterfacePage.py
+++ Plugins/FullUI/InterfacePage/InterfacePage.py
@@ -1,11 +1,17 @@
+import os
 from PyQt5.QtWidgets import QLineEdit,QLabel, QToolButton, QComboBox
 import Preferences
 
+from Preferences.ConfigurationPages.InterfacePage import InterfacePage
 from Preferences.ConfigurationPages import Ui_InterfacePage
 from Plugins.FullUI.InterfacePage import Ui_InterfacePage as New_Ui_InterfacePage
 from FullUI import UiHelper
 oldSetupUi = None
+oldSave = None
+oldInit = None
 
+
+#override Ui_InterfacePage
 def modifiedSetupUI(self, ui_interfacepage):    
     fakeLineEdit = QLineEdit()
     fakeLabel = QLabel()
@@ -16,23 +22,61 @@ def modifiedSetupUI(self, ui_interfacepage):
     self.styleSheetEdit.setReadOnly(True)
     self.styleSheetButton = fakeButton
     self.styleComboBox = fakeComboBox
+    self.styleComboBox.setReadOnly(True)
 
-    # setup new style box
-    
     oldSetupUi(self, ui_interfacepage)
+
+#override InterfacePage
+def save(self):
+    oldSave(self)
+    Preferences.setUI(
+            "StyleSheet",
+            getNewStyleSheet(self))
+
+#override InterfacePage
+def init(self):
+    oldInit(self)
+    fakeComboBox = QComboBox()
+    self.colorComboBox = fakeComboBox
+    self.__path = os.path.dirname(os.path.realpath(__file__))
+    populateColorStyleCombo(self)
+
+def getCurrentStyle():
+    curStyle = Preferences.getUI("StyleSheet")
+    if "style-light.qss" in curStyle:
+        return 'Light'
+    else:
+        return 'Dark'
+
+def getNewStyleSheet(self):
+    boxIndex = self.colorComboBox.currentIndex()
+    styleSheets = ['darkstyle/style.qss','darkstyle/style-light.qss']
+    return self.__path + "/PycomStyle/" + styleSheets[boxIndex]
+
+def populateColorStyleCombo(self):
+    curStyle = getCurrentStyle()
+    styles = ['Dark','Light']
+    for style in styles:
+        self.colorComboBox.addItem(style, style)
+    currentIndex = self.colorComboBox.findData(curStyle)
+    if currentIndex == -1:
+        currentIndex = 0
+    self.colorComboBox.setCurrentIndex(currentIndex)
 
 def hookInterfacePage():
     global oldSetupUi
-    
+    global oldSave
+    global oldInit
     Ui_InterfacePage.Ui_InterfacePage = New_Ui_InterfacePage.Ui_InterfacePage
     oldSetupUi = Ui_InterfacePage.Ui_InterfacePage.setupUi
     Ui_InterfacePage.Ui_InterfacePage.setupUi = modifiedSetupUI
 
+    if oldSave == None:
     
-    
-    # we must create fake widgets (hidden ones) to satisfy the original window content
-    # oldSetupUi = Ui_InterfacePage.Ui_InterfacePage.setupUi
-    # Ui_InterfacePage.Ui_InterfacePage.setupUi = modifiedSetupUI
-
+        #InterfacePage
+        oldSave = InterfacePage.save
+        oldInit = InterfacePage.__init__
+        InterfacePage.save = save
+        InterfacePage.__init__ = init
 
 hookInterfacePage() 
